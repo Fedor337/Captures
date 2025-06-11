fdec0b56bc44dd91618687d39ee431896d0c0047
Fedor337 <145130420+Fedor337@users.noreply.github.com>
2025-06-12 00:26:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import argparse
 from reference_preparer import ReferencePreparer
 from probe_generator import ProbeGenerator
-from temperature_filtered import main as filter_tm
+from temperature_filter import main as filter_tm
 from GC_filtered import main as filter_gc
-from structure_filtered import main as filter_structure
-from repetitions_filtered import main as filter_repeats
+from repetitions_filter import main as filter_repeats
 from pathlib import Path
 
 def main():
